9bf869a6a83d89ea50b9a90156944065cf236ebb
Mauricio Pereira <mauriciojuniordesigner@gmail.com>
2020-01-30 11:59:16 -0300

    adicionando codigo page_rank

--- consultas.py
+++ consultas.py
@@ -1,6 +1,51 @@
 import pymysql
 import nltk
 
+senha_db = 'senha'
+
+
+def calcula_page_rank(iteracoes):
+    conexao = pymysql.connect(
+        host='localhost', user='root', passwd=senha_db, db='indice', autocommit=True)
+    cursor_limpa_tabela = conexao.cursor()
+    cursor_limpa_tabela.execute('DELETE FROM page_rank')
+    cursor_limpa_tabela.execute('INSERT INTO page_rank SELECT idurl, 1.0 FROM urls')
+
+    for i in range(iteracoes):
+        print("Iteraçao " + str(i + 1))
+        cursor_url = conexao.cursor()
+        cursor_url.execute('SELECT idurl FROM urls')
+
+        for url in cursor_url:
+            pr = 0.15
+            cursor_links = conexao.cursor()
+            cursor_links.execute(
+                f'SELECT idurl_origem FROM url_ligacao WHERE idurl_destino = {url[0]}')
+
+            for link in cursor_links:
+                cursor_page_rank = conexao.cursor()
+                cursor_page_rank.execute(
+                    f'SELECT nota FROM page_rank WHERE idurl = {link[0]}')
+                link_page_rank = cursor_page_rank.fetchone()[0]
+                cursor_quantidade = conexao.cursor()
+                cursor_quantidade.execute(
+                    f'SELECT COUNT(*) FROM url_ligacao WHERE idurl_origem = {link[0]}')
+                link_quantidade = cursor_quantidade.fetchone()[0]
+                pr += 0.85 * (link_page_rank / link_quantidade)
+                cursor_quantidade.close()
+                cursor_page_rank.close()
+
+            cursor_atualiza = conexao.cursor()
+            cursor_atualiza.execute(
+                f'UPDATE page_rank SET nota = {pr} WHERE idurl = {url[0]}')
+            cursor_atualiza.close()
+            cursor_links.close()
+
+        cursor_url.close()
+
+    cursor_limpa_tabela.close()
+    conexao.close()
+
 
 def frequencia_score(linhas):
     contagem = dict((linha[0], 0) for linha in linhas)
@@ -18,12 +63,38 @@ def localizacao_score(linhas):
     return localizacoes
 
 
+def distancia_score(linhas):
+    if len(linhas[0]) <= 2:
+        return dict([(linha[0], 1.0) for linha in linhas])
+    distancias = dict([(linha[0], 1000000) for linha in linhas])
+    for linha in linhas:
+        dist = sum([abs(linha[i] - linha[i - 1]) for i in range(2, len(linha))])
+        if dist < distancias[linha[0]]:
+            distancias[linha[0]] = dist
+    return distancias
+
+
+def constagens_links_score(linhas):
+    contagem = dict((linha[0], 1) for linha in linhas)
+    conexao = pymysql.connect(
+        host='localhost', user='root', passwd=senha_db, db='indice')
+    cursor = conexao.cursor()
+    for i in contagem:
+        cursor.execute(f'SELECT COUNT(*) FROM url_ligacao WHERE idurl_destino = {i}')
+        contagem[i] = cursor.fetchone()[0]
+    cursor.close()
+    conexao.close()
+    return contagem
+
+
 def pesquisa(consulta):
     linhas, palavras_id = busca_mais_palavras(consulta)
     # scores = dict((linha[0], 0) for linha in linhas)
     # scores = frequencia_score(linhas)
-    scores = localizacao_score(linhas)
-    scores_ordenado = sorted([(score, url) for (url, score) in scores.items()], reverse=False)
+    # scores = localizacao_score(linhas)
+    # scores = distancia_score(linhas)
+    scores = constagens_links_score(linhas)
+    scores_ordenado = sorted([(score, url) for (url, score) in scores.items()], reverse=True)
     for (score, idurl) in scores_ordenado[0:10]:
         print(f'{score} \t {get_url(idurl)}')
 
@@ -31,7 +102,7 @@ def pesquisa(consulta):
 def get_url(idurl):
     retorno = ''
     conexao = pymysql.connect(
-        host='localhost', user='root', passwd='!@Jfpb2019', db='indice')
+        host='localhost', user='root', passwd=senha_db, db='indice')
     cursor = conexao.cursor()
     cursor.execute(f'SELECT url FROM urls WHERE idurl = {idurl}')
     if cursor.rowcount > 0:
@@ -64,7 +135,7 @@ def busca_mais_palavras(consulta):
     consulta_completa = f'SELECT {lista_campos} FROM {lista_tabelas} WHERE {lista_clausulas}'
 
     conexao = pymysql.connect(
-        host='localhost', user='root', passwd='!@Jfpb2019', db='indice')
+        host='localhost', user='root', passwd=senha_db, db='indice')
     cursor = conexao.cursor()
     cursor.execute(consulta_completa)
     linhas = [linha for linha in cursor]
@@ -78,7 +149,7 @@ def get_id_palavra(palavra):
     retorno = -1
     stemmer = nltk.stem.RSLPStemmer()
     conexao = pymysql.connect(
-        host='localhost', user='root', passwd='!@Jfpb2019', db='indice')
+        host='localhost', user='root', passwd=senha_db, db='indice')
     cursor = conexao.cursor()
     cursor.execute(
         f'SELECT idpalavra FROM palavras WHERE palavra = "{stemmer.stem(palavra)}"')
@@ -92,7 +163,7 @@ def get_id_palavra(palavra):
 def busca_uma_palavra(palavra):
     id_palavra = get_id_palavra(palavra)
     conexao = pymysql.connect(
-        host='localhost', user='root', passwd='!@Jfpb2019', db='indice')
+        host='localhost', user='root', passwd=senha_db, db='indice')
     cursor = conexao.cursor()
     cursor.execute(
         'SELECT urls.url FROM palavra_localizacao plc INNER JOIN' +
@@ -117,4 +188,6 @@ def busca_uma_palavra(palavra):
 # print(linhas)
 # print(palavras_id)
 
-pesquisa('python programação')
+# pesquisa('python programação')
+
+calcula_page_rank(20)
--- crawler.py
+++ crawler.py
@@ -6,14 +6,16 @@ import nltk
 import pymysql
 
 
+senha_db = 'senha'
+
+
 def insere_palavra_localizacao(id_url, id_palavra, localizacao):
     conexao = pymysql.connect(
-        host='localhost', user='root', passwd='!@Jfpb2019',
-        db='indice', autocommit=True)
+        host='localhost', user='root', passwd=senha_db, db='indice', autocommit=True)
     cursor = conexao.cursor()
     cursor.execute(
-        'insert into palavra_localizacao (idurl, idpalavra, localizacao) ' +
-        f'values ({id_url}, {id_palavra}, {localizacao})')
+        'INSERT INTO palavra_localizacao (idurl, idpalavra, localizacao) ' +
+        f'VALUES ({id_url}, {id_palavra}, {localizacao})')
     id_palavra_localizacao = cursor.lastrowid
     cursor.close()
     conexao.close()
@@ -22,41 +24,90 @@ def insere_palavra_localizacao(id_url, id_palavra, localizacao):
 
 def insere_palavra(palavra):
     conexao = pymysql.connect(
-        host='localhost',
-        user='root',
-        passwd='!@Jfpb2019',
-        db='indice',
-        autocommit=True,
-        use_unicode=True,
-        charset='utf8mb4'
+        host='localhost', user='root', passwd=senha_db, db='indice',
+        autocommit=True, use_unicode=True, charset='utf8mb4'
     )
     cursor = conexao.cursor()
-    cursor.execute(f'insert into palavras (palavra) values ("{palavra}")')
+    cursor.execute(f'INSERT INTO palavras (palavra) VALUES ("{palavra}")')
     id_palavra = cursor.lastrowid
     cursor.close()
     conexao.close()
     return id_palavra
 
 
+def insere_url_ligacao(id_url_origem, id_url_destino):
+    conexao = pymysql.connect(
+        host='localhost', user='root', passwd=senha_db, db='indice', autocommit=True)
+    cursor = conexao.cursor()
+    cursor.execute('INSERT INTO url_ligacao (idurl_origem, idurl_destino) ' +
+                   f'VALUES ("{id_url_origem}", "{id_url_destino}")')
+    id_url_ligacao = cursor.lastrowid
+    cursor.close()
+    conexao.close()
+    return id_url_ligacao
+
+
+def insere_url_palavra(id_palavra, id_url_ligacao):
+    conexao = pymysql.connect(
+        host='localhost', user='root', passwd=senha_db, db='indice', autocommit=True)
+    cursor = conexao.cursor()
+    cursor.execute('INSERT INTO url_palavra (idpalavra, idurl_ligacao) ' +
+                   f'VALUES ("{id_palavra}", "{id_url_ligacao}")')
+    id_url_palavra = cursor.lastrowid
+    cursor.close()
+    conexao.close()
+    return id_url_palavra
+
+
+def get_id_url_ligacao(id_url_origem, id_url_destino):
+    id_url_ligacao = -1
+    conexao = pymysql.connect(
+        host='localhost', user='root', passwd=senha_db, db='indice')
+    cursor = conexao.cursor()
+    cursor.execute('SELECT idurl_ligacao FROM url_ligacao WHERE ' +
+                   f'idurl_origem = {id_url_origem} AND ' +
+                   f'idurl_destino = {id_url_destino}')
+    if cursor.rowcount > 0:
+        id_url_ligacao = cursor.fetchone()[0]
+    cursor.close()
+    conexao.close()
+    return id_url_ligacao
+
+
+def get_id_url(url):
+    id_url = -1
+    conexao = pymysql.connect(
+        host='localhost', user='root', passwd=senha_db, db='indice',
+        autocommit=True, use_unicode=True, charset='utf8mb4'
+    )
+    cursor = conexao.cursor()
+    cursor.execute(f'SELECT idurl FROM urls WHERE url = {url}')
+    if cursor.rowcount > 0:
+        id_url = cursor.fetchone()[0]
+    cursor.close()
+    conexao.close()
+    return id_url
+
+
 def palavra_indexada(palavra):
     retorno = -1  # não existe a palavra
-    conexao = pymysql.connect(host='localhost', user='root', passwd='!@Jfpb2019', db='indice')
+    conexao = pymysql.connect(
+        host='localhost', user='root', passwd=senha_db, db='indice')
     cursor = conexao.cursor()
-    cursor.execute(f'select idpalavra from palavras where palavra = "{palavra}"')
+    cursor.execute(f'SELECT idpalavra FROM palavras WHERE palavra = "{palavra}"')
     if cursor.rowcount > 0:
-        # print('Palavra já cadastrada')
         retorno = cursor.fetchone()[0]
-    # else:
-    #     print('Palavra não cadastrada')
     cursor.close()
     conexao.close()
     return retorno
 
 
 def insere_pagina(url):
-    conexao = pymysql.connect(host='localhost', user='root', passwd='!@Jfpb2019', db='indice', autocommit=True)
+    conexao = pymysql.connect(
+        host='localhost', user='root', passwd=senha_db, db='indice',
+        autocommit=True, use_unicode=True, charset='utf8mb4')
     cursor = conexao.cursor()
-    cursor.execute(f'insert into urls (url) values ("{url}")')
+    cursor.execute(f'INSERT INTO urls (url) VALUES ("{url}")')
     id_pagina = cursor.lastrowid
     cursor.close()
     conexao.close()
@@ -66,27 +117,21 @@ def insere_pagina(url):
 def pagina_indexada(url):
     retorno = -1  # não existe a página
     conexao = pymysql.connect(
-        host='localhost', user='root', passwd='!@Jfpb2019', db='indice')
+        host='localhost', user='root', passwd=senha_db, db='indice')
     cursor_url = conexao.cursor()
-    cursor_url.execute(f'select idurl from urls where url = "{url}"')
+    cursor_url.execute(f'SELECT idurl FROM urls WHERE url = "{url}"')
 
     if cursor_url.rowcount > 0:
-        # print('Url encontrada')
         id_url = cursor_url.fetchone()[0]
         cursor_palavra = conexao.cursor()
         cursor_palavra.execute(
             f'SELECT idurl FROM palavra_localizacao WHERE idurl = "{id_url}"')
         if cursor_palavra.rowcount > 0:
-            # print('Url com palavras')
             retorno = -2  # existe a pagina com palavras cadastradas
         else:
             print('Url sem palavras')
             retorno = id_url  # existe a pagina sem palavras, retorna id da pagina
-
         cursor_palavra.close()
-    # else:
-    #     print('Url não cadastrada')
-
     cursor_url.close()
     conexao.close()
 
@@ -105,6 +150,30 @@ def separa_palavras(texto):
             if len(p) > 1:
                 lista_palavras.append(stemmer.stem(p).lower())
     return lista_palavras
+
+# link = 'https://pt.wikipedia.org/w/index.php?title=Linguagem_de_programa%C3%A7%C3%A3o&action=edit'
+# link = link.replace('_', ' ')
+# palavras = separa_palavras(link)
+# print(palavras)
+
+
+def url_liga_palavra(url_origem, url_destino):
+    texto_url = url_destino.replace('_', ' ')
+    palavras = separa_palavras(texto_url)
+    id_url_origem = get_id_url(url_origem)
+    id_url_destino = get_id_url(url_destino)
+    if id_url_destino == -1:
+        id_url_destino = insere_pagina(url_destino)
+    if id_url_origem == id_url_destino:
+        return
+    if get_id_url_ligacao(id_url_origem, id_url_destino) > 0:
+        return
+    id_url_ligacao = insere_url_ligacao(id_url_origem, id_url_destino)
+    for palavra in palavras:
+        id_palavra = palavra_indexada(palavra)
+        if id_palavra == -1:
+            id_palavra = insere_palavra(palavra)
+        insere_url_palavra(id_palavra, id_url_ligacao)
 
 
 def get_texto(sopa):
@@ -156,7 +225,7 @@ def crawl(paginas, profundidade):
                         url = url.split('#')[0]
                         if url[0:4] == 'http':
                             novas_paginas.add(url)
-
+                        url_liga_palavra(pagina, url)
                         contador += 1
 
                 paginas = novas_paginas
